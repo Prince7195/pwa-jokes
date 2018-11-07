f3fdcfa26eeb91e938d1a878bb45507bf4c6c943
Vijay Deepak <vijaydeepak7195@outlook.com>
2018-11-07 14:24:02 +0530

    pwa-jokes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-jokes';
+
+  update = false;
+  joke: any;
+
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.activated.subscribe(event => {
+      // this.update = true;
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.data.gimmeJokes().subscribe(res => {
+      this.joke = res;
+    });
+  }
 }
